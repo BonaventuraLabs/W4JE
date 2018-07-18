efc5f176ee8d7eab10baf73959cf642bc93e2950
Dmitry Ershov <dmitry.ershov@pasteur.fr>
2018-07-18 13:31:25 +0200

    Create Units module

---
+++ src/Units.py
@@ -0,0 +1,16 @@
+import pygame as pg
+
+
+class Unit(pg.sprite.Sprite):
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
