4fdf1ee970b1002340b6895ced623bb694f92e14
Dmitry Ershov <dimitracis@gmail.com>
2018-07-20 03:15:25 +0200

    Integrate Units and Main with Wind

--- src/main.py
+++ src/main.py
@@ -1,40 +1,67 @@
 import pygame as pg
 from src.settings import *
 import sys
-from src.Utilities import ImageLoader
-from src.MapUtilities import Map, Camera
-
+from src.Utilities import ImageLoader, Hud
+from src.MapUtilities import Map, Camera, Wind
+from src.Units import Ship
+from collections import deque
 
 class Game:
     def __init__(self):
         # init and create window
         pg.init()
+        pg.key.set_repeat(500, 50)
+        self.font = pg.font.SysFont("comicsansms", 20)
+
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
-        pg.key.set_repeat(1000, 100)
         self.clock = pg.time.Clock()
         self.images = ImageLoader(self)
+        self.sprites_anim = pg.sprite.Group()
         self.unit_sprites = pg.sprite.Group()
         self.map_sprites = pg.sprite.Group()
         self.all_sprites = pg.sprite.Group()
-        #TODO: the ship is added inside Map(). We need to decouple it from Map()
-        self.ship = None
+        self.top_layer_sprites = pg.sprite.Group()
+        self.hud = Hud(self)
         self.map = Map(self)
         self.camera = Camera(self.map.width, self.map.height)
+        self.player_deque = deque([Ship(self, 10, 10, 'Dimas', YELLOW),
+                                  Ship(self, 12, 12, 'Alex', RED),
+                                  Ship(self, 15, 15, 'Danila', GREEN)
+                                  ])
+        self.current_player = self.player_deque[0]
+        self.current_player.set_current()
+        self.message = ''
+        self.wind = Wind(self)
+
+        self.playing = True
+        self.turn_finished = False
 
     def run(self):
-        self.playing = True
-        # self.turn_finished = False
         while self.playing:
-            self.dt = self.clock.tick(FPS) / 1000
+            # self.dt = self.clock.tick(FPS) / 1000
             # Do animations continuously
-            # but movement of units - only on EndTurn!
-            self.events()
-            # if self.turn_finished:
-            self.update()
-                # self.turn_finished = False
+            self.update_animation()
 
             self.draw()
+
+            # check events, see if the turn is finished.
+            self.events()
+
+            # update some events; for instance, camera shift, or menu.
+            # self.update_some_events()
+
+            # if finished, update state.
+            if self.current_player.is_done:
+                #self.current_player.update()
+                self.update_state_on_turn()
+                # go to next player
+                self.player_deque.rotate(1)
+                self.current_player = self.player_deque[0]
+                self.current_player.set_current()
+
+    def update_animation(self):
+        self.sprites_anim.update()
 
     def events(self):
         for event in pg.event.get():
@@ -43,19 +70,26 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     self.quit()
-                # if event.key == pg.K_e:
-                #     self.turn_finished = True
+                else:
+                    self.current_player.check_key_input()
+                    self.camera.check_key_input()
 
-    def update(self):
-        self.all_sprites.update()
-        self.camera.update(self.ship)
+    def update_state_on_turn(self):
+        self.wind.get_new_direction()
+
 
     def draw(self):
         self.screen.fill(BGCOLOR)
         for sprite in self.map_sprites:
             self.screen.blit(sprite.image, self.camera.apply(sprite))
         for sprite in self.unit_sprites:
-            self.screen.blit(sprite.image, self.camera.apply(sprite))
+            sprite.draw()
+        for sprite in self.top_layer_sprites:
+            self.screen.blit(sprite.image, sprite)
+        label = self.font.render('TURN: ' + self.current_player.name, True, BLACK)
+        label_rect = label.get_rect(center=(200, HUD_HEIGHT/2))
+        self.screen.blit(label, label_rect)
+        self.wind.show_text()
         pg.display.flip()
 
     def show_start_screen(self):
